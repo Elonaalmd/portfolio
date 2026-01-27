764736556f9c0e31bcd410d22c253d5c68b9670f
Mateo Ribeiro <mateo.ribeiro314@gmail.com>
2026-01-27 15:24:34 +0100

    fixed script

--- asset/js/index_script.js
+++ asset/js/index_script.js
@@ -6,7 +6,7 @@ setInterval(spinner.first_to_back, 5000)
 
 let gallery = document.getElementById("gallery")
 gallery.first_to_back = function () {
-	spinner.appendChild(spinner['children'][0])
+	gallery.appendChild(gallery['children'][0])
 }
 
 setInterval(gallery.first_to_back, 5000)
